2f158ada4f59f6aec0021589f4b827a57d64061f
Morgan Moss <mmoss021@student.wethinkcode.co.za>
2022-09-12 16:24:21 +0200

    Added Routes to add request and for the action corresponding to it.

--- src/main/java/weshare/server/Routes.java
+++ src/main/java/weshare/server/Routes.java
@@ -11,11 +11,16 @@ public class Routes {
     public static final String LOGOUT = "/logout";
     public static final String EXPENSES = "/expenses";
 
+    public static final String PAYMENT_REQUEST_FORM = "/payment-request/{Expense_ID}";
+    public static final String SEND_PAYMENT_REQUEST = "/payment-request.action";
+
     public static void configure(WeShareServer server) {
         server.routes(() -> {
-            post(LOGIN_ACTION,  PersonController.login);
-            get(LOGOUT,         PersonController.logout);
-            get(EXPENSES,           ExpensesController.view);
+            post(LOGIN_ACTION,          PersonController.login);
+            get(LOGOUT,                 PersonController.logout);
+            get(EXPENSES,               ExpensesController.view);
+            get(PAYMENT_REQUEST_FORM,   PaymentRequestController.form);
+            post(SEND_PAYMENT_REQUEST,  PaymentRequestController.send);
         });
     }
 }
