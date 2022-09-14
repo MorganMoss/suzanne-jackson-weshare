7d8eb6cb34fdd992fb3f989ad55d218e7df5c017
kmoepya021 <kmoepya021@student.wethinkcode.co.za>
2022-09-15 00:20:27 +0200

    Payment Request Sent Page Created

---
+++ src/main/java/weshare/controller/PaymentRequestSentController.java
@@ -0,0 +1,56 @@
+package weshare.controller;
+
+import io.javalin.http.Handler;
+import org.javamoney.moneta.function.MonetaryFunctions;
+import org.jetbrains.annotations.NotNull;
+import weshare.model.Expense;
+import weshare.model.PaymentRequest;
+import weshare.model.Person;
+import weshare.persistence.ExpenseDAO;
+import weshare.server.ServiceRegistry;
+import weshare.server.WeShareServer;
+
+import javax.money.MonetaryAmount;
+import javax.money.NumberValue;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static weshare.model.MoneyHelper.ZERO_RANDS;
+import static weshare.model.MoneyHelper.amountOf;
+
+public class PaymentRequestSentController {
+
+    public static final Handler view = context -> {
+        ExpenseDAO expensesDAO = ServiceRegistry.lookup(ExpenseDAO.class);
+        Person personLoggedIn = WeShareServer.getPersonLoggedIn(context);
+
+        Collection<Expense> expenses = expensesDAO.findExpensesForPerson(personLoggedIn);
+        Collection<MonetaryAmount> netExpenseTotal = netExpensesList(expenses);
+        MonetaryAmount netExpensesTotal = netExpensesTotal(netExpenseTotal);
+
+        Map<String, Object> viewModel = new HashMap<>();
+        viewModel.put("expenses", expenses);
+        viewModel.put("netExpensesTotal", netExpensesTotal);
+
+        context.render("payment-request-sent.html", viewModel);
+    };
+
+    private static Collection<MonetaryAmount> netExpensesList(Collection<Expense> expenses) {
+        Collection<MonetaryAmount> netExpenses = new ArrayList<>();
+
+        for(Expense expense: expenses) netExpenses.add(expense.amountLessPaymentsReceived());
+
+        return netExpenses;
+    }
+
+    public static MonetaryAmount netExpensesTotal(Collection<MonetaryAmount> netExpensesList) {
+        int netExpenseInt = amountOf(0).getNumber().intValue();
+
+        for(MonetaryAmount netExpense: netExpensesList) netExpenseInt += netExpense.getNumber().intValue();
+
+        return amountOf(netExpenseInt);
+    }
+}
--- src/main/java/weshare/server/Routes.java
+++ src/main/java/weshare/server/Routes.java
@@ -11,6 +11,7 @@ public class Routes {
     public static final String LOGOUT = "/logout";
     public static final String EXPENSES = "/expenses";
 
+    public static final String PAYMENT_REQUEST_SENT = "/paymentrequests_sent";
     public static final String PAYMENT_REQUEST_FORM = "/payment-request";
     public static final String SEND_PAYMENT_REQUEST = "/payment-request.action";
 
@@ -23,9 +24,10 @@ public class Routes {
             post(LOGIN_ACTION,          PersonController.login);
             get(LOGOUT,                 PersonController.logout);
             get(EXPENSES,               ExpensesController.view);
+            get(PAYMENT_REQUEST_SENT,   PaymentRequestSentController.view);
             get(PAYMENT_REQUEST_FORM,   PaymentRequestController.form);
-            post(SEND_PAYMENT_REQUEST,  PaymentRequestController.send);
             get(NEW_EXPENSE,            NewExpensesController.view);
+            post(SEND_PAYMENT_REQUEST,  PaymentRequestController.send);
             post(SAVE_EXPENSE,          NewExpensesController.saveExpense);
         });
     }
